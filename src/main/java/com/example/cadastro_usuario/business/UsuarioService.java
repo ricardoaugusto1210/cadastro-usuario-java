2a24515a99a0412805c70d4791aba1def1eb430d
ricardoaugusto1210 <ricardoaugusto.eb@gmail.com>
2026-01-16 00:30:25 -0300

    crud complet

--- src/main/java/com/example/cadastro_usuario/business/UsuarioService.java
+++ src/main/java/com/example/cadastro_usuario/business/UsuarioService.java
@@ -27,8 +27,19 @@ public class UsuarioService {
         repository.deleteByEmail(email);
     }
 
-    public void atualizarUsuarioPorEmail(String email, Usuario usuario){
-        Usuario usuarioEntity = buscarUsuarioPorEmail(email);
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario){
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado")
+        );
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ?
+                        usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ?
+                        usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+
+        repository.saveAndFlush(usuarioAtualizado);
 
     }
 }
